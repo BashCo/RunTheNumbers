a0dd612ecd0ee10571a8c6632926db0d53275220
BashCo <BashCo@users.noreply.github.com>
2020-08-16 16:48:48 +0200

    Update runthenumbers.sh
    adding changes by @SylTi to main file for WSL compatibility.

--- runthenumbers.sh
+++ runthenumbers.sh
@@ -8,7 +8,7 @@ IP_ADDRESS="127.0.0.1"
 PORT="8332"
 
 # This is the block height we're waiting for
-TARGET_BLOCK="644000"
+TARGET_BLOCK="645000"
 
 OUTPUT_FILE="the_numbers_${TARGET_BLOCK}.txt"
 
@@ -19,20 +19,18 @@ LATEST_BLOCK="0"
 while true
 do
     # Check that USERNAME and PASSWORD are filled in
-    if [ "$USERNAME" == "RPC_USERNAME" ] || [ "$PASSWORD" == "RPC_PASSWORD" ]; then 
+    if [ $USERNAME = "RPC_USERNAME" ] || [ $PASSWORD = "RPC_PASSWORD" ]; then
         echo "first you need to update the username and password located in the script header to match those in your bitcoin.conf file"
         exit
     fi
 
-    # This fetches the current block height from your full node. 
-    CURRENT_BLOCK="$(curl -s --user "${USERNAME}":"${PASSWORD}" --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockcount", "params": [] }' -H 'content-type: text/plain;' http://${IP_ADDRESS}:${PORT}/ | \
-    python -c "import sys, json; print(json.load(sys.stdin)['result'])")"
+    # This fetches the current block height from your full node.
+    CURRENT_BLOCK=$(curl -s --user "${USERNAME}":"${PASSWORD}" --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockcount", "params": [] }' -H 'content-type: text/plain;' http://${IP_ADDRESS}:${PORT}/ | python -c "import sys, json; print(json.load(sys.stdin)['result'])")
 
     # If the block height matches, call gettxoutsetinfo and print to file
-    if [ "$CURRENT_BLOCK" == "$TARGET_BLOCK" ]; then
+    if [ $CURRENT_BLOCK = $TARGET_BLOCK ]; then
         echo "$CURRENT_BLOCK/$TARGET_BLOCK: running the numbers...";
-        TXOUTSETINFO="$(curl -s --user "${USERNAME}":"${PASSWORD}" --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "gettxoutsetinfo", "params": [] }' -H 'content-type: text/plain;' http://${IP_ADDRESS}:${PORT}/ | \
-        python -c "import sys, json; parsed = json.load(sys.stdin)['result']; print(json.dumps(parsed, indent=2))")"
+        TXOUTSETINFO=$(curl -s --user "${USERNAME}":"${PASSWORD}" --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "gettxoutsetinfo", "params":[] }' -H 'content-type: text/plain;' http://${IP_ADDRESS}:${PORT}/ | python -c "import sys, json; parsed = json.load(sys.stdin)['result']; print(json.dumps(parsed, indent=2))")
         echo
         echo
         echo "${TXOUTSETINFO}"
@@ -43,22 +41,22 @@ do
         exit
 
     # If block height doesn't match yet, update console when a new block is found
-    elif [ "$CURRENT_BLOCK" -lt "$TARGET_BLOCK" ] && [ "$CURRENT_BLOCK" -gt "$LATEST_BLOCK" ]; then
-        LATEST_BLOCK="$CURRENT_BLOCK"
+    elif [ $CURRENT_BLOCK -lt $TARGET_BLOCK ] && [ $CURRENT_BLOCK -gt $LATEST_BLOCK ]; then
+        LATEST_BLOCK=$CURRENT_BLOCK
         REMAINING=$(($TARGET_BLOCK - $CURRENT_BLOCK))
         BLOCKS="blocks"
-        if [ "$REMAINING" == 1 ]; then
+        if [ $REMAINING = 1 ]; then
             BLOCKS="block"
         fi
         echo "$CURRENT_BLOCK/$TARGET_BLOCK: ${REMAINING} ${BLOCKS} remaining"
-        
+
     # If block height has already passed, go home and do better next time
-    elif [ "$CURRENT_BLOCK" -gt "$TARGET_BLOCK" ]; then
+    elif [ $CURRENT_BLOCK -gt $TARGET_BLOCK ]; then
         echo "${TARGET_BLOCK} has already passed. current block is ${CURRENT_BLOCK}";
         exit
 
     fi
-    
+
     # Query block height every n seconds
     sleep 5
 
